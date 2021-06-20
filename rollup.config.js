85bc6eeb8a1b4336e515dd11ad87489ef3f7012d
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-06-20 14:22:55 -0400

    feat: add non-minified `./dist/WebGLazy.js`

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,46 @@
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
-import {
-	terser
-} from "rollup-plugin-terser";
-
+import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
-export default {
-	input: './src/main',
-	output: [{
-		file: pkg.main,
-		format: 'cjs',
-	}, {
-		file: pkg.module,
-		format: 'es',
-	}, {
-		file: './dist/WebGLazy.min.js',
-		name: 'WebGLazy',
-		format: 'iife',
-	}],
-	plugins: [
-		babel({
-			exclude: 'node_modules/**',
-		}),
-		commonjs(),
-		terser(),
-	],
-}
+export default [
+	{
+		input: './src/main',
+		output: [
+			{
+				file: pkg.main,
+				format: 'cjs',
+			},
+			{
+				file: pkg.module,
+				format: 'es',
+			},
+			{
+				file: './dist/WebGLazy.min.js',
+				name: 'WebGLazy',
+				format: 'iife',
+			},
+		],
+		plugins: [
+			babel({
+				exclude: 'node_modules/**',
+			}),
+			commonjs(),
+			terser(),
+		],
+	},
+	{
+		input: './src/main',
+		output: {
+			file: './dist/WebGLazy.js',
+			name: 'WebGLazy',
+			format: 'iife',
+		},
+		plugins: [
+			babel({
+				exclude: 'node_modules/**',
+			}),
+			commonjs(),
+		],
+	},
+];
